c4094d70f82becfedea7f0d738cca3638836e41e
Datorium <hello@datorium.lv>
2021-01-14 17:06:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankingApp_202101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
