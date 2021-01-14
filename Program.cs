b7b8b053d7458d69f6e2df46658535f9f482932d
Datorium <hello@datorium.lv>
2021-01-14 18:39:04 +0200

    version 18:38

--- Program.cs
+++ Program.cs
@@ -7,16 +7,18 @@ namespace BankingApp_202101
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("***Welcome to my Banking App***");
+            Console.WriteLine("***Welcome Digital Banking App***");
 
             List<BankAccount> accounts = new List<BankAccount>();
 
-            accounts.Add(new BankAccount("BA123456789", "Anna Lopez", 5000));
-            accounts.Add(new BankAccount("BA123456790", "John Salamander", 200));
-            accounts.Add(new BankAccount("BA123456791", "Jenifer Poles", 150000));           
+            accounts.Add(new BankAccount("BA123456789", "Anna Lopez", 5000, "EUR"));
+            accounts.Add(new BankAccount("BA123456790", "John Salamander", 200, "USD"));
+            accounts.Add(new BankAccount("BA123456791", "Jenifer Poles", 150000, "GBP"));
 
-
-
+            foreach (var account in accounts)
+            {
+                account.Print();
+            }
         }
     }
 
@@ -25,17 +27,21 @@ namespace BankingApp_202101
         public string Number { get; }
         public string Owner { get; }
         public decimal Balance { get; }
+        public string Currency { get; }
+        public DateTime DateCreated { get; }
 
-        public BankAccount(string number, string owner, decimal balance)
+        public BankAccount(string number, string owner, decimal balance, string currency)
         {
             this.Number = number;
             this.Owner = owner;
             this.Balance = balance;
+            this.Currency = currency;
+            this.DateCreated = DateTime.Now;
         }
 
         public void Print()
         {
-            Console.WriteLine($"Account number {this.Number} belongs to {this.Owner} and has a balance of {this.Balance.ToString()} euros");
+            Console.WriteLine($"Account {this.Number} created on {this.DateCreated} belongs to {this.Owner} and has a balance of {this.Balance.ToString()} {this.Currency}");
         }
     }
 }
