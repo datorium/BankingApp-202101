00fda52068bf4f6d61441c34eebac9f943b30683
Datorium <hello@datorium.lv>
2021-01-16 12:57:32 +0200

    version 12:57

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BankingApp_202101
 {
@@ -6,7 +7,42 @@ namespace BankingApp_202101
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***Datorium Digital Bank***");
+            Console.WriteLine("");
+
+            List<BankAccount> accounts = new List<BankAccount>();
+
+            accounts.Add(new BankAccount("DDB123456789", "Linus Torvalds", 4000000, "EUR"));          
+            accounts.Add(new BankAccount("DDB123456790", "Anna Kurnikova", 10000000, "RUB"));          
+            accounts.Add(new BankAccount("DDB123456791", "Jenifer Lopez", 200, "USD"));
+           
+            foreach(var account in accounts)
+            {
+                account.Print();
+            }
+        }
+    }
+
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; }
+        public decimal Balance { get; }
+        public string Currency { get; }
+        public DateTime DateCreated { get; }
+
+        public BankAccount(string number, string owner, decimal balance, string currency)
+        {
+            this.Number = number;
+            this.Owner = owner;
+            this.Balance = balance;
+            this.Currency = currency;
+            this.DateCreated = DateTime.Now;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"- account {this.Number} created on {this.DateCreated} belongs to {this.Owner} and has a balance of {this.Balance} {this.Currency}");
         }
     }
 }
